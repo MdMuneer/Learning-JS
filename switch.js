bb16d7e0cd722cd510969ae00468b0c462a69669
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-06 20:08:45 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,19 @@
+function  caseInSwitch(val) {
+	var answer= "";
+	switch(val) {
+	case 1:
+	  answer  = "apple";
+	  break;
+	case 2:
+	  answer  = "apple";
+	  break;
+	default:
+	   answer ="empty"
+	   break;
+
+	}
+
+	return answer;
+} 
+
+console.log(caseInSwitch());
