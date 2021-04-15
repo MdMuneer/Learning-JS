416e16c9d67d8b10f252379b5a2938b881f50d94
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-15 18:23:40 +0530

    Delete basic.js

--- basic.js
+++
@@ -1,7 +0,0 @@
-function wordBlanks(myNoun, myAjective, myVerb, myAdverb){
-	var result="";
-	result += "the "  + myAjective + "  " + myNoun + " " + myVerb +" " + " to the store " + myAdverb
-	return result;
-}
-
-console.log(wordBlanks("dog","big","ran","fast"))
