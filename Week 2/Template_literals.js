b347f87c87b9f76ec86a851ffcdf4bf1c7b91bdd
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-15 18:35:35 +0530

    Add files via upload

---
+++ Week 2/Template_literals.js
@@ -0,0 +1,9 @@
+const person = {
+	name:"Muneer",
+	age:22
+}
+// literals arebackticks {advanatge : can make multi line strings, add single or double quotes.can add js using $//
+const greeting = `Hello, my name ${person.name}! 
+I am ${person.age} years old.`;
+
+console.log(greeting);
