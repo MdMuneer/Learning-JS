935ec9aeb669d5b7e2d2fdf383c88cd332a0f53d
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-15 18:22:10 +0530

    Add files via upload

---
+++ Week 1/loops.js
@@ -0,0 +1,13 @@
+var myArray =  [];
+
+for (var i=0; i<5; i++) {  //first is initialization, condition , and final expression//
+	myArray.push(i);
+}
+
+
+
+for (var i=10; i>0; i-=2) {  //first is initialization, condition , and final expression//
+	myArray.push(i);
+}
+
+console.log(myArray)
