a16c7d2344ea268d27c3a7ba9cdca42aa0c5881f
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-24 16:59:18 +0530

    Update spread_operator.js

--- Week 2/spread_operator.js
+++ Week 2/spread_operator.js
@@ -1,7 +1,7 @@
 const arr1=['jan','feb','may','oct'];
 let arr2
 (function() {
-	arr2 = [...arr1]; 		//spread operator for spreading an array here spreading contents of arr1 to arr2//
+	arr2 = [...arr1]; 		//spread operator for spreading an array here spreading contents of arr1 to arr2///
 	arr1[0] ='potato'
 })();
 console.log(arr2);
