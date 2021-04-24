00239ac0b322b6792af875dece714e405c3c4813
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-24 16:59:49 +0530

    Update Template_literals.js

--- Week 2/Template_literals.js
+++ Week 2/Template_literals.js
@@ -2,7 +2,7 @@ const person = {
 	name:"Muneer",
 	age:22
 }
-// literals arebackticks {advanatge : can make multi line strings, add single or double quotes.can add js using $//
+// literals are backticks (advanatge : can make multi line strings, add single or double quotes.can add js using $)//
 const greeting = `Hello, my name ${person.name}! 
 I am ${person.age} years old.`;
 
