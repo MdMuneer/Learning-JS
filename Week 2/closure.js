175287cf5f41825557392b49d1d6a56a0596822d
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-05-17 23:18:24 +0530

    Update closure.js

--- Week 2/closure.js
+++ Week 2/closure.js
@@ -3,7 +3,7 @@ function x(){
 	function y(){
 		console.log(a);
 	}
-	a=500;		//will print 500 while returning y because a does not refer to 7 ,a's reference(a memory location) is returned/
+	a=500;		//will print 500 while returning y because a does not refer to 7 ,a's reference(a memory location) is returned//
 	return y;
 }
 var z=x();
